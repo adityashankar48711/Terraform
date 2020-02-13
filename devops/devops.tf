cbd48e85eefe912aad13afa8ce5a246819378b8a
adityashankar48711 <60744266+adityashankar48711@users.noreply.github.com>
2020-02-14 03:35:24 +0530

    Update devops.tf

--- devops/devops.tf
+++ devops/devops.tf
@@ -34,15 +34,4 @@ resource "null_resource" "remote-exec-1" {
   }
 }
 
-resource "null_resource" "ansible-main" {
-provisioner "local-exec" {
-  command = <<EOT
-        sleep 100;
-        > jenkins-ci.ini;
-        echo "[jenkins-ci]"| tee -a jenkins-ci.ini;
-        export ANSIBLE_HOST_KEY_CHECKING=False;
-        echo "${aws_instance.backend.public_ip}" | tee -a jenkins-ci.ini;
-        ansible-playbook  --key=${var.pvt_key} -i jenkins-ci.ini web-playbook.yaml -u ubuntu -v
-    EOT
-}
-}
+
